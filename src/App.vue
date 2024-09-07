6500f4a9ba1ce8b2d416b777045ec19859e53149
HyeonJi Tak <jirae1997@naver.com>
2024-09-07 21:58:21 +0900

    feat: composition api 활용법

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,31 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted, ref, watch } from "vue";
+import ChildComp from "./components/ChildComp.vue";
+
+let count = ref(0);
+let title = "Hello Vue";
+let color = ref("red");
+
+onMounted(() => {
+    console.log("mounted!");
+});
+watch(color, () => {
+    console.log("color!!");
+});
+
+const increaseCount = () => {
+    count.value++;
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <div>
+        <h1>{{ title }}</h1>
+        <button @click="increaseCount">count++</button>
+        <p>{{ count }}</p>
+        <ChildComp :color="color" bgColor="yellow" />
+        <button @click="color = 'green'">change color</button>
+    </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
---
+++ src/components/ChildComp.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h2>Child Componet</h2>
+        <p :style="{ color: color, backgroundColor: bgColor }">{{ color }}</p>
+    </div>
+</template>
+<script setup>
+const props = defineProps({
+    color: String,
+    bgColor: String,
+});
+</script>
+<style></style>
