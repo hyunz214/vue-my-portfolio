ec830d641b240993490594a268d1a67337bf278b
HyeonJi Tak <jirae1997@naver.com>
2024-11-11 22:39:51 +0900

    test: test입니다!

--- src/components/ChildComp.vue
+++ src/components/ChildComp.vue
@@ -2,6 +2,7 @@
     <div>
         <h2>Child Componet</h2>
         <p :style="{ color: color, backgroundColor: bgColor }">{{ color }}</p>
+        <p :style="{ color: color, backgroundColor: bgColor }">test!!!</p>
     </div>
 </template>
 <script setup>
